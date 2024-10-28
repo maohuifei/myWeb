73dd97fe7628390e9615b103d751622d7784f23a
maohuifei <991270685@qq.com>
2024-10-28 19:11:41 +0800

    favicon、loading

--- clientDir/userInterface/src/App.vue
+++ clientDir/userInterface/src/App.vue
@@ -1,25 +1,23 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <div class="tage_box">
-    <div class="logo_box">
-      <RouterLink to="/">
-        <img class="logo_class" src="/portrait.png" alt="Logo" />
-      </RouterLink>
-    </div>
-    <div class="nav_box">
-      <RouterLink to="/">首页</RouterLink>
-      <RouterLink to="/article">文章</RouterLink>
-      <RouterLink to="/about">关于</RouterLink>
-      <RouterLink to="/privacy">公告</RouterLink>
-    </div>
-    <div class="icon_box">
-      <svg class="icon" aria-hidden="true">
-        <use href="#icon-sousuo"></use>
-      </svg>
-      <!-- <svg class="icon" aria-hidden="true">
+  <div v-if="isLoading" class="boxLoading"></div>
+  <div v-else>
+    <div class="tage_box">
+      <div class="logo_box">
+        <RouterLink to="/">
+          <img class="logo_class" src="/portrait.png" alt="Logo" />
+        </RouterLink>
+      </div>
+      <div class="nav_box">
+        <RouterLink to="/">首页</RouterLink>
+        <RouterLink to="/article">文章</RouterLink>
+        <RouterLink to="/about">关于</RouterLink>
+        <RouterLink to="/privacy">公告</RouterLink>
+      </div>
+      <div class="icon_box">
+        <svg class="icon" aria-hidden="true">
+          <use href="#icon-sousuo"></use>
+        </svg>
+        <!-- <svg class="icon" aria-hidden="true">
         <use href="#icon-shengyin"></use>
       </svg>
       <svg class="icon" aria-hidden="true">
@@ -28,18 +26,34 @@ import { RouterLink, RouterView } from 'vue-router'
       <svg class="icon" aria-hidden="true">
         <use href="#icon-dingyue"></use>
       </svg> -->
+      </div>
+    </div>
+    <div style="height: 60px;"></div>
+    <div class="content_box">
+      <RouterView />
+    </div>
+    <div class="foot_box">
+      <p>© 2024 huafeng 版权所有</p>
+      <p>备案号：鲁ICP备2024118017号 </p>
     </div>
   </div>
-  <div style="height: 60px;"></div>
-  <div class="content_box">
-    <RouterView />
-  </div>
-  <div class="foot_box">
-    <p>© 2024 huafeng 版权所有</p>
-    <p>备案号：鲁ICP备2024118017号 </p>
-  </div>
 </template>
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import { ref, onMounted } from 'vue';
+export default {
+  setup() {
+    const isLoading = ref(true)
+    onMounted(() => [
+      isLoading.value = false
+    ])
+    return {
+      isLoading,
+    }
+  }
+}
 
+</script>
 <style scoped lang="less">
 .tage_box {
   width: 100%;
@@ -59,9 +73,10 @@ import { RouterLink, RouterView } from 'vue-router'
   }
 
   .logo_box {
-    a:hover{
-      background-color:transparent;
+    a:hover {
+      background-color: transparent;
     }
+
     .logo_class {
       width: 130px;
     }
@@ -97,5 +112,79 @@ import { RouterLink, RouterView } from 'vue-router'
   padding-top: 15px;
   margin: 0 auto;
   color: var(--txtColor);
+}
+
+//加载动画
+.boxLoading {
+  width: 50px;
+  height: 50px;
+  margin: auto;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+
+//方块影子
+.boxLoading:before {
+  content: '';
+  width: 50px;
+  height: 5px;
+  background: rgb(107, 107, 107);
+  opacity: 0.7;
+  position: absolute;
+  top: 59px;
+  left: 0;
+  border-radius: 50%;
+  animation: shadow .5s linear infinite;
+}
+
+//方块本体
+.boxLoading:after {
+  content: '';
+  width: 50px;
+  height: 50px;
+  background: rgb(45, 197, 239);
+  animation: animate .5s linear infinite;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-radius: 3px;
+}
+
+@keyframes animate {
+  17% {
+    border-bottom-right-radius: 3px;
+  }
+
+  25% {
+    transform: translateY(9px) rotate(22.5deg);
+  }
+
+  50% {
+    transform: translateY(18px) scale(1, .9) rotate(45deg);
+    border-bottom-right-radius: 40px;
+  }
+
+  75% {
+    transform: translateY(9px) rotate(67.5deg);
+  }
+
+  100% {
+    transform: translateY(0) rotate(90deg);
+  }
+}
+
+@keyframes shadow {
+
+  0%,
+  100% {
+    transform: scale(1, 1);
+  }
+
+  50% {
+    transform: scale(1.2, 1);
+  }
 }
 </style>
